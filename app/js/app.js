2fbf5da863b596c043656a3c3ba66871404e623f
Nagaraju <nagarajueac12@gmail.com>
2017-07-09 12:44:10 +0530

    Updated responsive and images

--- app/js/app.js
+++ app/js/app.js
@@ -158,27 +158,15 @@
           'imageLocations': [
             'css/imgs/bg_pattern.jpg',
             'css/imgs/banner.jpg',
-            'files/1.jpg',
+            'files/1_t.jpg',
             'files/2_t.jpg',
             'files/3_t.jpg',
-            'files/5.jpg',
-            'files/6.jpg',
-            'files/7.jpg',
-            'files/8.jpg',
-            'files/9.jpg',
-            'files/10.jpg',
-            'files/11.jpg',
+            'files/6_t.jpg',
+            'files/9_t.jpg',
             'files/12_t.jpg',
             'files/13_t.jpg',
-            'files/14.jpg',
-            'files/15.jpg',
-            'files/16.jpg',
-            'files/17.jpg',
-            'files/18.jpg',
-            'files/19.jpg',
-            'files/20.jpg',
-            'files/21.jpg',
-            'files/22.jpg'
+            'files/20_t.jpg',
+            'files/22_t.jpg'
           ]
         });
 
@@ -234,8 +222,8 @@
 angular.module('classicevents').run(['$templateCache', function($templateCache) {$templateCache.put('source/js/components/about/about.html','<div class="classic-events about" id="about">\n  <div class="wrapper">\n    <h2 class="title">About Us</h2>\n    <p>\n      We are premium decorators for all occasion happens with your family, we make your occasion colourful, joyful, and theme based elegant decors as per your dreams within your budget and our relationship starts here. </p>\n    <p>We do traditional, modern and contemporary styles of d\xE9cor as per your Occasion.</p>\n    <div>\n      <div class="about-block">\n        <h3>Why to call us</h3>\n        <p>Occasions come to celebrate our emotions and happiness, the beauty of happiness and emotions are displayed in the form of d\xE9cor which makes your dreams to be shared with your family.</p>\n        <p> Our team make your dreams come true with new different designs and unique style as to be a remembrance for every one of your special occasion.</p>\n      </div>\n      <div class="about-block">\n        <h3>Our Decor Services</h3>\n        <ul class="list-style">\n          <li>\n            <h4>Banquet halls in all Hotels and Resorts</h4>\n            <p>(Confluence, GRT, ITC, My Fortune, Taj Connemara, Taj Coromandal, Leela Palace, Hilton, Radisson blu, Hyatt, Crown Plaza, Feathers, Taj Fishcove, Turya, Trident, Westin etc\u2026.)</p>\n          </li>\n          <li>\n            <h4>All wedding halls</h4>\n            <p>(MRC Kalyana mandapam, Raja Muthiah, Rani Meyyammai etc\u2026)</p>\n          </li>\n          <li>\n            <h4>Outdoor lawns, Farm houses, houses</h4>\n            <p>(VGP, Royal palms, Green coconut resort, Shelter beach resort etc\u2026)</p>\n          </li>\n        </ul>\n\n      </div>\n      <div class="about-block last-block">\n        <h3>What all we do</h3>\n        <ul class="list-style">\n          <li>Engagement d\xE9cor</li>\n          <li>Sangeet &amp; Mehendi decor</li>\n          <li>Muhurtha mandapam &amp; Dining decor</li>\n          <li>Reception, Entrance &amp; Passage decor</li>\n          <li>First night room d\xE9cor &amp; car decor</li>\n          <li>Birthday party</li>\n          <li>Get together party</li>\n          <li>Casino Party &amp; Pool Party</li>\n          <li>Corporate party</li>\n          <li>Cocktail, DJ and Entertainment party</li>\n          <li>And all theme parties</li>\n        </ul>\n      </div>\n    </div>\n  </div>\n</div>\n');
 $templateCache.put('source/js/components/contact/contact.html','<div class="classic-events" id="contact">\n  <h2>Contact us</h2>\n  <div class="wrapper">\n    <div class="contact-info">\n      <div class="contact-info-block">\n        <h4>Call</h4>\n        <ul>\n          <li><a href="#" title="+91-9790999569">+91-9790999569</a></li>\n        </ul>\n      </div>\n      <!-- <div class="contact-info-block">\n        <h4>Social</h4>\n        <ul class="inline">\n          <li>\n            <a href="#"></a>\n          </li>\n          <li>\n            <a href="#"></a>\n          </li>\n          <li>\n            <a href="#"></a>\n          </li>\n        </ul>\n      </div> -->\n      <div class="contact-info-block">\n          <h4>Email</h4>\n          <ul>\n            <li><a href="#">subashraguram@gmail.com</a></li>\n            <li><a href="#">classiceventschennai@gmail.com</a></li>\n          </ul>\n      </div>\n    </div>\n  </div>\n</div>\n');
 $templateCache.put('source/js/components/gallery/gallery.html','<div class="classic-events gallery" id="gallery">\n  <h2 class="title">Gallery</h2>\n  <div class="wrapper">\n    <ce-gallery-item></ce-gallery-item>\n  </div>\n</div>\n');
-$templateCache.put('source/js/components/testimonials/testimonials.html','<div class="classic-events testimonials" id="testimonials">\n  <h2 class="title">Testimonials</h2>\n  <div class="wrapper">\n    <div class="testimonial-wrapper">\n      <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>\n      <p>Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>\n    </div>\n\n  </div>\n</div>\n');
 $templateCache.put('source/js/components/home/home.html','<div class="classic-events home">\n    <!-- <div class="header-top">\n        <div class="wrapper">\n            <ul>\n                <li><a href="#" title="subashraguram@gmail.com">classiceventschennai@gmail.com</a></li>\n                <li><a href="#" title="9790999569">9790999569</a></li>\n            </ul>\n        </div>\n    </div> -->\n    <div ng-if="home.imageLoaded">\n        <ce-header></ce-header>\n        <!-- Banner section -->\n        <div class="wrapper mobile">\n          <ul class="">\n              <li><a href="#" title="subashraguram@gmail.com">classiceventschennai@gmail.com</a></li>\n              <li><a href="#" title="9790999569">9790999569</a></li>\n          </ul>\n        </div>\n        <ce-banner></ce-banner>\n        <ce-about></ce-about>\n        <ce-gallery></ce-gallery>\n        <!-- <ce-testimonials></ce-testimonials> -->\n        <ce-contact></ce-contact>\n    </div>\n    <div ng-if="home.imageLoaded !== true" class="home-loader">\n        <div class="windows8 loader">\n            <div class="wBall" id="wBall_1">\n                <div class="wInnerBall"></div>\n            </div>\n            <div class="wBall" id="wBall_2">\n                <div class="wInnerBall"></div>\n            </div>\n            <div class="wBall" id="wBall_3">\n                <div class="wInnerBall"></div>\n            </div>\n            <div class="wBall" id="wBall_4">\n                <div class="wInnerBall"></div>\n            </div>\n            <div class="wBall" id="wBall_5">\n                <div class="wInnerBall"></div>\n            </div>\n        </div>\n    </div>\n</div>\n');
+$templateCache.put('source/js/components/testimonials/testimonials.html','<div class="classic-events testimonials" id="testimonials">\n  <h2 class="title">Testimonials</h2>\n  <div class="wrapper">\n    <div class="testimonial-wrapper">\n      <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>\n      <p>Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>\n    </div>\n\n  </div>\n</div>\n');
 $templateCache.put('source/js/directives/banner/banner.html','<div class="classic-events banner">\n    <div class="wrapper">\n        <img src="css/imgs/banner.jpg" alt="Classic Event decor" />\n        <div class="wrapper-logo"></div>\n    </div>\n</div>\n');
 $templateCache.put('source/js/directives/footer/footer.html','');
 $templateCache.put('source/js/directives/gallery-item/gallery.item.html','<div class="classic-events gallery-item">\n    <ul>\n      <li><img src="files/1_t.jpg" alt="Gallery 1" ce-colorbox image-url="files/1.jpg" id="gallery-item-1"></li>\n      <li><img src="files/2_t.jpg" alt="Gallery 2" ce-colorbox image-url="files/2.jpg" id="gallery-item-2"></li>\n      <li><img src="files/3_t.jpg" alt="Gallery 3" ce-colorbox image-url="files/3.jpg" id="gallery-item-3"></li>\n    <!-- </ul>\n    <ul> -->\n      <!-- <li><img src="files/5.jpg" alt="Gallery 4" ce-colorbox image-url="files/5.jpg" id="gallery-item-4"></li> -->\n      <li><img src="files/6_t.jpg" alt="Gallery 5" ce-colorbox image-url="files/6.jpg" id="gallery-item-5"></li>\n      <!-- <li><img src="files/7.jpg" alt="Gallery 6" ce-colorbox image-url="files/7.jpg" id="gallery-item-6"></li> -->\n    <!-- </ul>\n    <ul> -->\n      <!-- <li><img src="files/8.jpg" alt="Gallery 7" ce-colorbox image-url="files/8.jpg" id="gallery-item-7"></li> -->\n      <li><img src="files/9_t.jpg" alt="Gallery 8" ce-colorbox image-url="files/9.jpg" id="gallery-item-8"></li>\n      <!-- <li><img src="files/10.jpg" alt="Gallery 9" ce-colorbox image-url="files/10.jpg" id="gallery-item-9"></li> -->\n    <!-- </ul>\n    <ul> -->\n      <!-- <li><img src="files/11.jpg" alt="Gallery 10" ce-colorbox image-url="files/11.jpg" id="gallery-item-10"></li> -->\n      <li><img src="files/12_t.jpg" alt="Gallery 11" ce-colorbox image-url="files/12.jpg" id="gallery-item-11"></li>\n      <li><img src="files/13_t.jpg" alt="Gallery 12" ce-colorbox image-url="files/13.jpg" id="gallery-item-12"></li>\n    <!-- </ul>\n    <ul> -->\n      <!-- <li><img src="files/14.jpg" alt="Gallery 13" ce-colorbox image-url="files/14.jpg" id="gallery-item-13"></li>\n      <li><img src="files/15.jpg" alt="Gallery 14" ce-colorbox image-url="files/15.jpg" id="gallery-item-14"></li>\n      <li><img src="files/16.jpg" alt="Gallery 15" ce-colorbox image-url="files/16.jpg" id="gallery-item-15"></li> -->\n    <!-- </ul>\n    <ul> -->\n      <!-- <li><img src="files/17.jpg" alt="Gallery 16" ce-colorbox image-url="files/17.jpg" id="gallery-item-16"></li>\n      <li><img src="files/18.jpg" alt="Gallery 17" ce-colorbox image-url="files/18.jpg" id="gallery-item-17"></li>\n      <li><img src="files/19.jpg" alt="Gallery 18" ce-colorbox image-url="files/19.jpg" id="gallery-item-18"></li> -->\n    <!-- </ul>\n    <ul> -->\n      <li><img src="files/20_t.jpg" alt="Gallery 19" ce-colorbox image-url="files/20.jpg" id="gallery-item-19"></li>\n      <!-- <li><img src="files/21.jpg" alt="Gallery 20" ce-colorbox image-url="files/21.jpg" id="gallery-item-20"></li> -->\n      <li><img src="files/22_t.jpg" alt="Gallery 21" ce-colorbox image-url="files/22.jpg" id="gallery-item-21"></li>\n    </ul>\n</div>\n');
@@ -450,6 +438,35 @@ $templateCache.put('source/js/directives/menu/menu.html','');}]);
             });
     }
 
+})();
+
+;
+(function() {
+    angular
+        .module('classicevents')
+        .directive('ceTestimonials', ceTestimonials)
+        .controller('CETestimonialsController', CETestimonialsController);
+
+    function ceTestimonials() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {},
+            templateUrl: 'source/js/components/testimonials/testimonials.html',
+            controller: CETestimonialsController,
+            controllerAs: 'home',
+            bindToController: true
+        };
+    }
+
+    function CETestimonialsController($state, preloader, appConstants) {
+
+        var vm = this;
+        initialize();
+        function initialize() {
+
+        }
+    }
 })();
 
 ;
@@ -862,33 +879,4 @@ $templateCache.put('source/js/directives/menu/menu.html','');}]);
       }
     };
   }
-})();
-
-;
-(function() {
-    angular
-        .module('classicevents')
-        .directive('ceTestimonials', ceTestimonials)
-        .controller('CETestimonialsController', CETestimonialsController);
-
-    function ceTestimonials() {
-        return {
-            restrict: 'E',
-            replace: true,
-            scope: {},
-            templateUrl: 'source/js/components/testimonials/testimonials.html',
-            controller: CETestimonialsController,
-            controllerAs: 'home',
-            bindToController: true
-        };
-    }
-
-    function CETestimonialsController($state, preloader, appConstants) {
-
-        var vm = this;
-        initialize();
-        function initialize() {
-
-        }
-    }
 })();
